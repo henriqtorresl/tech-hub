417c13ac842c496f42f7ee4b4462382d31c28f86
henriqtorresl <henriquetlandin@gmail.com>
2024-08-26 20:24:55 -0300

    adicionando o corpo da pagina de profile..

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['auth']);
+  }
 
 }
