384da85cec267582dea6c06321452a80322b7212
henriqtorresl <henriquetlandin@gmail.com>
2024-08-29 12:45:43 -0300

    corpo da home page finalizado

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/interfaces/post';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  posts: Post[] = [];
+
+  constructor(
+    private postService: PostService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.postService.getAll().subscribe((response) => {
+      this.posts = response;
+    });
+  }
 
 }
